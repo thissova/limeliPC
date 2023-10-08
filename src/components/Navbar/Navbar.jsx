407dc2c838309c2385b3f65ea33a289f70d04f18
Ivan Savchenko <ivansavchenkogym555@gmail.com>
2023-10-08 22:07:23 +0300

    navbar + background

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import logo from "../../assets/logo.png"
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
+    return (<div className={styles.mainDiv}>
+            <div>
+                <img src={logo} alt={"logo"} className={styles.logo}/>
+            </div>
+
+            <div className={styles.breadCrumb}>
+                <a href={"#home"}>Home</a>
+            </div>
+
+            <div className={styles.breadCrumb}>
+                <a href={"#about-us"}>About us</a>
+            </div>
+            <div className={styles.breadCrumb}>
+                <a href={"#our-works"}>Our works</a>
+            </div>
+            <div className={styles.breadCrumb}>
+                <a href={"#purchase"}>Purchase now</a>
+            </div>
+
+
         </div>
     );
 };
