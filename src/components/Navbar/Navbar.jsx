19eb99a6b7d1ee370f200bebbf77afca6918ca8f
Ivan Savchenko <ivansavchenkogym555@gmail.com>
2023-10-09 10:13:24 +0300

    changed font + optimized for 4k monitors

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from "../../assets/logo.png"
 import styles from './Navbar.module.css'
+import {RiCloseLine, RiMenu3Line} from "react-icons/ri";
+import classnames from "classnames";
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false)
     return (<div className={styles.mainDiv}>
             <div>
                 <img src={logo} alt={"logo"} className={styles.logo}/>
@@ -21,6 +24,22 @@ const Navbar = () => {
             <div className={styles.breadCrumb}>
                 <a href={"#purchase"}>Purchase now</a>
             </div>
+            <div className={styles.menu}>
+                {toggleMenu
+                    ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/>
+                    : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>}
+                {toggleMenu && (
+                    <div className={classnames("scale-up-center", styles.menuBlock)}>
+                        <div className={classnames(styles.menuBlockLinks)}>
+                            <p><a href={"#home"}>Home</a></p>
+                            <p><a href={"#about-us"}>About us</a></p>
+                            <p><a href={"#our-works"}>Our works</a></p>
+                            <p><a href={"#purchase"}>Purchase now</a></p>
+                        </div>
+                    </div>
+                )}
+            </div>
+
 
 
         </div>
