0c42a2298313f04c8773d613dc67db25d8468690
Ivan Savchenko <ivansavchenkogym555@gmail.com>
2023-10-17 23:09:52 +0300

    completed site

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import styles from "./Footer.module.css"
 
 const Footer = () => {
     return (
-        <div id={"purchase-footer"}>
-            Footer
+        <div className={styles.mainDiv} id={"purchase-footer"}>
+
+            <div className={styles.title}>
+                <h1>Order now your gaming PC</h1>
+            </div>
+            <a className={styles.button} href={"https://t.me/LimeliDS"} target={"_blank"}><button >Our contacts</button></a>
+            <div className={styles.copyright}><p>©Limeli PC team</p></div>
         </div>
     );
 };
